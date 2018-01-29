a4d2cb5d279b8d1e0cf465b884557dc9275cebfe
Tim Dresser <tdresser@chromium.org>
2018-01-29 14:40:25 -0500

    Initial commit

---
+++ first-input-delay.js
@@ -0,0 +1,62 @@
+"use strict";
+
+(function() {
+  // TODO - only register touchstart / pointerdown if other listeners are present.
+  const eventTypes = ['mousedown', 'keydown', 'touchstart', 'pointerdown'];
+  let has_dispatched_delay_event = false;
+
+  function dispatchDelayEvent(delay) {
+    if (has_dispatched_delay_event)
+      return;
+    has_dispatched_delay_event = true;
+
+    const firstInputDelayEvent = new CustomEvent('first-input', {detail: {delay}});
+    document.dispatchEvent(firstInputDelayEvent);
+
+    for (let eventType of eventTypes) {
+      document.removeEventListener(eventType, onInput, {passive:true, capture:true});
+    }
+  }
+
+  // Pointer events can trigger main or compositor thread behavior. We attempt
+  // to differenciate these cases based on whether or not we see a
+  // pointercancel. pointercancels are fired when we scroll. If we scroll, we
+  // don't consider this a real input. This will count a scroll against a
+  // scroll extent as user input.
+  function processPointerDown(delay) {
+    function removeListeners() {
+      document.removeEventListener('pointerup', processPointerUp, {passive:true, capture:true});
+      document.removeEventListener('pointercancel', processPointerCancel, {passive:true, capture:true});
+    }
+
+    function processPointerUp() {
+      dispatchDelayEvent(delay);
+      removeListeners();
+    }
+
+    function processPointerCancel() {
+      removeListeners();
+    }
+
+    document.addEventListener('pointerup', processPointerUp, {passive:true, capture:true});
+    document.addEventListener('pointercancel', processPointerCancel, {passive:true, capture:true});
+  }
+
+  function onInput(event) {
+    // Only count cancelable events, which should trigger behavior important to the user.
+    if (event.cancelable == false)
+      return;
+
+    const delay = performance.now() - event.timeStamp;
+
+    if (event.type == 'pointerdown') {
+      processPointerDown(delay);
+      return;
+    }
+
+    dispatchDelayEvent(delay);
+  }
+
+  for (let eventType of eventTypes)
+    document.addEventListener(eventType, onInput, {passive:true, capture:true});
+})();
